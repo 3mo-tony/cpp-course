3668a3f8675552220f5500fdd35e2bd2f93a761b
TonyBuffon <antoniosamy@proton.me>
2025-10-25 00:25:13 +0300

    first session

---
+++ session-1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+// entry point of the program
+int main() {
+
+    cout << "Hello World" << endl;
+    cout << "Hello Tony" << endl;
+    cout << "I hope you are doing well and enjoying the course\n" << endl;
+
+    // comments are ignored by the compiler
+    // double holds pi variable
+    double pi = 3.14;
+    
+    // calculate the area of a circle
+    cout << pi * 3 * 3 << endl;
+    return 0;
+}
